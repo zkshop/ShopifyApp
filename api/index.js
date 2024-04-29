00d98331d3044d9ad3b9e6089ee11df9f1b58ea8
ebedoise <emilebedoiseau@gmail.com>
2024-04-29 10:23:03 +0200

    added cors

--- api/index.js
+++ api/index.js
@@ -23,11 +23,11 @@ const STATIC_PATH =
 const app = express();
 
 // CORS configuration
-// const corsOptions = {
-//   origin: "*",
-//   optionsSuccessStatus: 200
-// };
-// app.use(cors(corsOptions));
+const corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 
 // Set up Shopify authentication and webhook handling
 app.get(shopify.config.auth.path, shopify.auth.begin());
