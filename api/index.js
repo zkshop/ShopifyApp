9c64c8a95d44f87cd59e415f8f6b036496828c74
ebedoise <emilebedoiseau@gmail.com>
2024-04-29 10:50:53 +0200

    added cors

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ const app = express();
 
 // CORS configuration
 const corsOptions = {
-  origin: "*",
+  origin: ["https://admin.shopify.com/*"],
   optionsSuccessStatus: 200
 };
 app.use(cors(corsOptions));
