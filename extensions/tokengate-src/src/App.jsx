5c54f6eedc323c6ea7887f3c22977e9d6689fe1f
ebedoise <emilebedoiseau@gmail.com>
2024-05-06 11:58:24 +0200

    Indentation in extension

--- extensions/tokengate-src/src/App.jsx
+++ extensions/tokengate-src/src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState, useContext } from "react";
-import axios from 'axios';
 
 // XRP
 import { XamanWalletProvider, RenderQrCode } from './XamanWalletProvider';
@@ -36,7 +35,7 @@ const _App = () => {
       setIsOwner(false);
     else
       setIsOwner(true);
-};
+  };
   
   useEffect(() => {
     handleNftSearchOwner();
@@ -44,19 +43,19 @@ const _App = () => {
   
   useEffect(() => {
     if (auth.address !== undefined)
-    setWallet({ address: auth.address });
-  else
-  setWallet({ address: null });
-}, [auth.address]);
+      setWallet({ address: auth.address });
+    else
+      setWallet({ address: null });
+  }, [auth.address]);
 
-const handleConnectWallet = () => {
-  if (wallet.address === null) {
-    setShowQR(true);
-  }
-};
+  const handleConnectWallet = () => {
+    if (wallet.address === null) {
+      setShowQR(true);
+    }
+  };
 
-const handleDisconnectWallet = () => {
-  if (showQR)
+  const handleDisconnectWallet = () => {
+    if (showQR)
       setShowQR(false);
     if (wallet.address !== null)
       handlers.disconnect();
@@ -64,30 +63,30 @@ const handleDisconnectWallet = () => {
       handlers.close();
   };
     
-    useEffect(() => {
-      callGetNfts();
-    }, []);
+  useEffect(() => {
+    callGetNfts();
+  }, []);
     
-    const callGetNfts = async () => {
-      const nftsData = await XRPNftsReader().getNfts();
+  const callGetNfts = async () => {
+    const nftsData = await XRPNftsReader().getNfts();
       
-      if (nftsData.nfts.length > 0) {
-        for (let i = 0; i < nftsData.nfts.length; i++) {
-          const selectedNft = nftsData.nfts[i];
-          if (selectedNft && selectedNft.url) {
-            if (selectedNft.metadata && selectedNft.metadata.image && selectedNft.metadata.image.startsWith('ipfs://')) {
-              setNftImage(`https://cloudflare-ipfs.com/ipfs/${selectedNft.metadata.image.slice(7)}`);
-              break;
-            } else if (selectedNft.metadata && selectedNft.metadata.image_url) {
-              setNftImage(`https://cloudflare-ipfs.com/ipfs/${selectedNft.metadata.image_url.slice(12)}`);
-              break;
-            }
+    if (nftsData.nfts.length > 0) {
+      for (let i = 0; i < nftsData.nfts.length; i++) {
+        const selectedNft = nftsData.nfts[i];
+        if (selectedNft && selectedNft.url) {
+          if (selectedNft.metadata && selectedNft.metadata.image && selectedNft.metadata.image.startsWith('ipfs://')) {
+            setNftImage(`https://cloudflare-ipfs.com/ipfs/${selectedNft.metadata.image.slice(7)}`);
+            break;
+          } else if (selectedNft.metadata && selectedNft.metadata.image_url) {
+            setNftImage(`https://cloudflare-ipfs.com/ipfs/${selectedNft.metadata.image_url.slice(12)}`);
+            break;
           }
         }
-      } else {
-        setNftImage(null);
       }
-    };
+    } else {
+      setNftImage(null);
+    }
+  };
 
   useEffect(() => {
     if (buttons) {
@@ -103,24 +102,24 @@ const handleDisconnectWallet = () => {
   
   return (
     <div>
-        <div style={{border: '1px solid #ccc', borderRadius: '10px', padding: '10px', margin: '10px', display: 'flex', justifyContent: 'space-between', alignItems: 'center', flexDirection: 'column', position: 'relative'}}>
-          <div style={{display: 'flex', alignItems: 'center', marginBottom: '5px', marginLeft: '10px', flexDirection: 'column'}}>
-            <h2 style={{marginBottom: '5px', marginTop: '0'}}>{requirements?.conditions?.[0]?.name}</h2>
-            <div>
-              {isOwner ? <h3 style={{ color: 'green', marginTop: '5px' }}>content unlocked</h3> : <h3 style={{ color: 'red', marginTop: '5px' }}>content locked</h3>}
-            </div>
+      <div style={{border: '1px solid #ccc', borderRadius: '10px', padding: '10px', margin: '10px', display: 'flex', justifyContent: 'space-between', alignItems: 'center', flexDirection: 'column', position: 'relative'}}>
+        <div style={{display: 'flex', alignItems: 'center', marginBottom: '5px', marginLeft: '10px', flexDirection: 'column'}}>
+          <h2 style={{marginBottom: '5px', marginTop: '0'}}>{requirements?.conditions?.[0]?.name}</h2>
+          <div>
+            {isOwner ? <h3 style={{ color: 'green', marginTop: '5px' }}>content unlocked</h3> : <h3 style={{ color: 'red', marginTop: '5px' }}>content locked</h3>}
           </div>
-          {showQR && <RenderQrCode />}
-          <div style={{display: 'flex', justifyContent: 'center', marginTop: '5px', width: '90%'}}>
-            {showQR === false ?
-              <button style={{padding: '5px 10px', borderRadius: '5px', backgroundColor: '#007bff', color: '#fff', border: 'none', width: '100%', cursor: 'pointer', transition: 'background-color 0.3s ease-in-out'}} onClick={handleConnectWallet}>Connect your Wallet</button>
-              :
-              <button style={{padding: '5px 10px', borderRadius: '5px', backgroundColor: 'red', color: '#fff', border: 'none', width: '100%', cursor: 'pointer', transition: 'background-color 0.3s ease-in-out'}} onClick={handleDisconnectWallet}>{wallet.address === null ? 'Cancel' : `Disconnect ${wallet.address.slice(0, 4)}...${wallet.address.slice(-4)}`}</button>
-            }
-          </div>
-          {nftImage && <img src={nftImage} alt="NFT" style={{ maxWidth: '50px', maxHeight: '50px', borderRadius: '50%', position: 'absolute', top: '-10px', right: '-10px' }} />}
         </div>
+        {showQR && <RenderQrCode />}
+        <div style={{display: 'flex', justifyContent: 'center', marginTop: '5px', width: '90%'}}>
+          {showQR === false ?
+            <button style={{padding: '5px 10px', borderRadius: '5px', backgroundColor: '#007bff', color: '#fff', border: 'none', width: '100%', cursor: 'pointer', transition: 'background-color 0.3s ease-in-out'}} onClick={handleConnectWallet}>Connect your Wallet</button>
+            :
+            <button style={{padding: '5px 10px', borderRadius: '5px', backgroundColor: 'red', color: '#fff', border: 'none', width: '100%', cursor: 'pointer', transition: 'background-color 0.3s ease-in-out'}} onClick={handleDisconnectWallet}>{wallet.address === null ? 'Cancel' : `Disconnect ${wallet.address.slice(0, 4)}...${wallet.address.slice(-4)}`}</button>
+          }
+        </div>
+        {nftImage && <img src={nftImage} alt="NFT" style={{ maxWidth: '50px', maxHeight: '50px', borderRadius: '50%', position: 'absolute', top: '-10px', right: '-10px' }} />}
       </div>
+    </div>
   );
 };
 
