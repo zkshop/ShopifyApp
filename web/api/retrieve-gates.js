b1c739894c02dfc1eccd7ca21aa6572632f4778f
Ilya <ilya@MacBook-Pro-Pavel.local>
2024-07-30 15:01:27 +0200

    little fix with logs

--- web/api/retrieve-gates.js
+++ web/api/retrieve-gates.js
@@ -36,7 +36,6 @@ const GATES_QUERY = `
 
 export default async function retrieveGates(session) {
   const client = new shopify.api.clients.Graphql({ session });
-  console.log('----> retrieveGates')
   try {
     const gates = await client.query({
       data: {
