089fce0bc9efee714d6b5edc5ced03aa9c5e2e53
Ilya <ilya@MacBook-Pro-Pavel.local>
2024-08-01 10:29:40 +0200

    change billing config

--- web/shopify.js
+++ web/shopify.js
@@ -8,11 +8,11 @@ const DB_PATH = `${process.cwd()}/database.sqlite`;
 // The transactions with Shopify will always be marked as test transactions, unless NODE_ENV is production.
 // See the ensureBilling helper to learn more about billing in this template.
 const billingConfig = {
-  "My Shopify One-Time Charge": {
+  "My Shopify Charge": {
     // This is an example configuration that would do a one-time charge for $5 (only USD is currently supported)
-    amount: 5.0,
+    amount: 9.99,
     currencyCode: "USD",
-    interval: BillingInterval.OneTime,
+    interval: BillingInterval.Every30Days, 
   },
 };
 
